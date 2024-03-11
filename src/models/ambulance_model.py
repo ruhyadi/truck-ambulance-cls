08c01e6baa1b31138ebc0a859557ae674d9c00f7
Didi Ruhyadi <ruhyadi.dr@gmail.com>
2024-03-11 07:35:56 +0000

    fix: model logging

--- src/models/ambulance_model.py
+++ src/models/ambulance_model.py
@@ -55,7 +55,7 @@ class AmbulanceLightningModel(LightningModule):
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         """Forward pass."""
         return self.model(x)
-    
+
     def on_train_start(self) -> None:
         """Actions to perform on train start."""
         log.info(f"Training started")
@@ -66,37 +66,55 @@ class AmbulanceLightningModel(LightningModule):
         """Training/validation step."""
         x, y = batch
         y_hat = self(x)
+
         loss = self.criterion(y_hat, y)
 
         y_hat = torch.argmax(y_hat, dim=1)
         y = torch.argmax(y, dim=1)
 
         return loss, y_hat, y
-    
+
     def training_step(self, batch: torch.Tensor, batch_idx: int) -> torch.Tensor:
         """Training step."""
         loss, y_hat, y = self.step(batch)
         self.train_loss(loss)
         self.train_accuracy(y_hat, y)
-        self.log("train/loss", self.train_loss, on_step=True, on_epoch=True)
-        self.log("train/acc", self.train_accuracy, on_step=True, on_epoch=True)
+
+        self.log_dict(
+            {
+                "train/loss": self.train_loss,
+                "train/acc": self.train_accuracy,
+            },
+            on_step=True,
+            on_epoch=True,
+            prog_bar=True,
+        )
 
         return loss
-    
+
     def validation_step(self, batch: torch.Tensor, batch_idx: int) -> torch.Tensor:
         """Validation step."""
         loss, y_hat, y = self.step(batch)
         self.val_loss(loss)
         self.val_accuracy(y_hat, y)
-        self.log("val/loss", self.val_loss, on_step=True, on_epoch=True)
-        self.log("val/acc", self.val_accuracy, on_step=True, on_epoch=True)
+
+        self.log_dict(
+            {
+                "val/loss": self.val_loss,
+                "val/acc": self.val_accuracy,
+            },
+            on_step=True,
+            on_epoch=True,
+            prog_bar=True,
+        )
 
         return loss
-    
+
     def configure_optimizers(self) -> optim.Optimizer:
         """Configure optimizer."""
         optimizer = optim.Adam(self.parameters(), lr=self.hparams.lr)
         return optimizer
+
 
 if __name__ == "__main__":
     """Debugging."""
